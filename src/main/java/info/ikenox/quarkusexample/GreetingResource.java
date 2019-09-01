f9f65020fc0dd505072639bc40b8bc4296a5ee93
Naoto Ikeno <naoto.ikeno@dena.com>
2019-09-01 14:45:33 +0900

    init

---
+++ src/main/java/info/ikenox/quarkusexample/GreetingResource.java
@@ -0,0 +1,23 @@
+package info.ikenox.quarkusexample;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String postHello() {
+        return "helloooooo";
+    }
+}
